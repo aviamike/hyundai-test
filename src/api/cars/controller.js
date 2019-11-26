b3606ab4800de5539e5b44e1ddef3d40ad4b259a
Mikhail Khusnudinov <mikh.khu@gmail.com>
2019-11-26 09:58:51 +0300

    fixed back-front interaction. working draft

--- src/api/cars/controller.js
+++ src/api/cars/controller.js
@@ -1,36 +1,57 @@
-import { success, notFound } from '../../services/response/'
-import { Cars } from '.'
+import { success, notFound } from "../../services/response/";
+import { Cars } from ".";
 
 export const create = ({ bodymen: { body } }, res, next) =>
   Cars.create(body)
-    .then((cars) => cars.view(true))
+    .then(cars => cars.view(true))
     .then(success(res, 201))
-    .catch(next)
+    .catch(next);
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   Cars.find(query, select, cursor)
-    .then((cars) => cars.map((cars) => cars.view()))
+    .then(cars => cars.map(cars => cars.view()))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
 export const show = ({ params }, res, next) =>
   Cars.findById(params.id)
     .then(notFound(res))
-    .then((cars) => cars ? cars.view() : null)
+    .then(cars => (cars ? cars.view() : null))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
 export const update = ({ bodymen: { body }, params }, res, next) =>
   Cars.findById(params.id)
     .then(notFound(res))
-    .then((cars) => cars ? Object.assign(cars, body).save() : null)
-    .then((cars) => cars ? cars.view(true) : null)
+    .then(async cars => {
+      if (cars) {
+        Object.assign(cars, body);
+        cars.reservations = cars.reservations[0].split(",");
+        return await cars.save();
+      } else {
+        return null;
+      }
+    })
+    .then(cars => (cars ? cars.view(true) : null))
     .then(success(res))
-    .catch(next)
+    .catch(next);
+
+// export const update = async ({ bodymen: { body }, params }, res, next) => {
+//   try {
+//     let car = await Cars.findById(params.id);
+//     body.reservations = await body.reservations.split(",");
+
+//     car = { ...car, ...body };
+//     console.log(car.reservations);
+//     await car.save();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export const destroy = ({ params }, res, next) =>
   Cars.findById(params.id)
     .then(notFound(res))
-    .then((cars) => cars ? cars.remove() : null)
+    .then(cars => (cars ? cars.remove() : null))
     .then(success(res, 204))
-    .catch(next)
+    .catch(next);
